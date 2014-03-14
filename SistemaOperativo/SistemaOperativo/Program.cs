b72ebe3be9a661fa55b9606eb719b91a773f4d83
Erick Castillo de la Garza <ecastillo550@gmail.com>
2014-03-14 12:03:09 -0600

    Code added

---
+++ SistemaOperativo/SistemaOperativo/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaOperativo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            String path = "C:\\Users\\ecast_000\\Documents\\NetBeansProjects\\SistemaOperativo\\src\\sistemaoperativo\\proc.txt";
+            pcb procesos = SetProcesos(path);
+            procesos.showState();
+        
+            Console.WriteLine("\n---------------------------------------------------------");
+            procesos.LFU(3, 3);
+            procesos.showState();
+        }
+
+      public static pcb SetProcesos(String path) {
+          pcb procesos = new pcb();
+      
+          try {
+             // Lectura del fichero
+             string[] lines = System.IO.File.ReadAllLines(@path);
+             String linea;
+             int pointer = 0;
+             String[] current;
+
+             linea = lines[pointer].Trim();
+             procesos.setMaxPag(int.Parse(linea));
+             pointer++;
+         
+             linea = lines[pointer].Trim();
+             procesos.setNumProcesos(int.Parse(linea));
+             pointer++;
+         
+             int id, llegada, tiempo, estado, numpaginas;
+         
+             for(int numproc = 1 ; numproc <= procesos.getNumProcesos(); numproc++){
+                 linea = lines[pointer].Trim();
+                 current = linea.Split(',');
+                 id = numproc;
+                 llegada = int.Parse(current[0].Trim());
+                 tiempo = int.Parse(current[1].Trim());
+                 estado = int.Parse(current[2].Trim());
+                 pointer++;
+             
+                 linea = lines[pointer].Trim();
+                 numpaginas = int.Parse(linea);
+                 procesos.setProceso(id, llegada, tiempo, estado, numpaginas);
+                 pointer++;
+             
+                 for(int numpag = 0 ; numpag <= procesos.getLastProceso().getNumeropaginas()-1; numpag++){
+                    linea = lines[pointer].Trim();
+                    current = linea.Split(',');
+                    procesos.getLastProceso().setPagina(numpag,
+                                                        int.Parse(current[0].Trim()),
+                                                        int.Parse(current[1].Trim()),
+                                                        int.Parse(current[2].Trim()),
+                                                        int.Parse(current[3].Trim()),
+                                                        int.Parse(current[4].Trim()),
+                                                        int.Parse(current[5].Trim()));
+                    pointer++;
+                 }
+             }
+          }
+          catch(Exception e) {
+             Console.WriteLine("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Error de archivo>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+             e.ToString();
+          } 
+        procesos.setTiempo();
+    
+        return procesos;
+        } // fin Setprocesos  
+    }
+}
---
+++ SistemaOperativo/SistemaOperativo/pcb.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaOperativo
+{
+    class pcb
+    {
+        private Proceso proceso;
+        private int maxPag;
+        private int numProcesos;
+        private int tiempo;
+
+        public void setTiempo()
+        {
+            Pagina auxpaginacion = new Pagina();
+            Proceso auxproceso = new Proceso();
+            int tiempomax = 0;
+            auxproceso = proceso;
+
+            while (auxproceso != null)
+            {
+                auxpaginacion = auxproceso.getListaPagina().getPagina();
+
+                while (auxpaginacion != null)
+                {
+                    if (auxpaginacion.getAcceso() > tiempomax)
+                    {
+                        tiempomax = auxpaginacion.getAcceso();
+                    }
+
+                    auxpaginacion = auxpaginacion.getNextPagina();
+                }
+                auxproceso = auxproceso.getNextProceso();
+            } // fin proceso while
+
+            this.tiempo = tiempomax;
+        }
+
+        public void setTiempo(int tiempo)
+        {
+            this.tiempo = tiempo;
+        }
+        public void TiempoPasa()
+        {
+            this.tiempo += 1;
+        }
+        public void setMaxPag(int maxPag)
+        {
+            this.maxPag = maxPag;
+        }
+        public void setNumProcesos(int numProcesos)
+        {
+            this.numProcesos = numProcesos;
+        }
+
+        public void setProceso(int id, int llegada, int tiempo, int estado, int numpag)
+        {
+            if (proceso == null)
+            {
+                proceso = new Proceso(id, llegada, tiempo, estado, numpag);
+            }
+            else
+            {
+                Proceso nuevoProceso = new Proceso(id, llegada, tiempo, estado, numpag);
+                Proceso aux = new Proceso();
+                aux = proceso;
+
+                while (aux.getNextProceso() != null)
+                {
+                    aux = aux.getNextProceso();
+                }
+                aux.setNextProceso(nuevoProceso);
+                //proceso = aux;
+            }
+        }
+
+        public Proceso getProceso()
+        {
+            return proceso;
+        }
+        public int getMaxPag()
+        {
+            return maxPag;
+        }
+        public int getTiempo()
+        {
+            return tiempo;
+        }
+        public int getNumProcesos()
+        {
+            return numProcesos;
+        }
+
+        public Proceso getLastProceso()
+        {
+            Proceso aux = new Proceso();
+            aux = proceso;
+
+            while (aux.getNextProceso() != null)
+            {
+                aux = aux.getNextProceso();
+            }
+            return aux;
+        }
+
+        public Proceso getProcesoByID(int id)
+        {
+            Proceso aux = new Proceso();
+            aux = proceso;
+
+            while (aux.getNextProceso() != null)
+            {
+                if (aux.getId() == id)
+                {
+                    return aux;
+                }
+                aux = aux.getNextProceso();
+            }
+            return aux;
+        }
+
+        public void showState(){
+        Pagina auxpaginacion = new Pagina();
+        Proceso auxproceso = new Proceso();
+        
+        auxproceso = proceso;
+        Console.WriteLine("SO max pag: " + this.getMaxPag());
+        Console.WriteLine("SO Num procesos: " + this.getNumProcesos());
+        Console.WriteLine("SO tiempo del sistema: " + this.getTiempo());
+        
+        while(auxproceso != null){
+            Console.WriteLine("\n---------------------------------------------------------");
+            Console.WriteLine("Proceso id: " + auxproceso.getId());
+            Console.WriteLine("Proceso llegada: " + auxproceso.getLlegada());
+            Console.WriteLine("Proceso estado: " + auxproceso.getEstado());
+            Console.WriteLine("Proceso num paginas : " + auxproceso.getNumeropaginas());
+            Console.WriteLine("Proceso tiempo estimado: " + auxproceso.getTiempo());
+
+            auxpaginacion = auxproceso.getListaPagina().getPagina();
+
+            while(auxpaginacion != null){
+                Console.WriteLine("\nProceso pagina numero: " + auxpaginacion.getNumero());
+                Console.WriteLine("Proceso pagina residencia: " + auxpaginacion.getResidencia());
+                Console.WriteLine("Proceso pagina llegada: " + auxpaginacion.getLlegada());
+                Console.WriteLine("Proceso pagina acceso : " + auxpaginacion.getAcceso());
+                Console.WriteLine("Proceso pagina numero de accesos : " + auxpaginacion.getNumAcceso());
+                Console.WriteLine("Proceso pagina NURlectura: " + auxpaginacion.getNURlectura());
+                Console.WriteLine("Proceso pagina NURescritura: " + auxpaginacion.getNURescritura());
+                Console.WriteLine("Proceso pagina modificacion: " + auxpaginacion.getModificacion()); 
+                auxpaginacion = auxpaginacion.getNextPagina();
+            }
+            auxproceso = auxproceso.getNextProceso();
+        } // fin proceso while
+    }
+
+        public void FIFO(int ProcesoID, int PaginaID)
+        {
+            int counter = 0;
+            int lowerID = 0;
+            int lower = 0;
+            Pagina aux = new Pagina();
+            aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+            if (this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getResidencia() == 0)
+            {
+                while (aux != null)
+                {
+                    if (aux.getResidencia() == 1)
+                    {
+                        counter++;
+                    }
+                    //hacemos crecer al maximo
+                    if (aux.getLlegada() > lower)
+                    {
+                        lower = aux.getLlegada();
+                    }
+                    aux = aux.getNextPagina();
+                }
+                if (counter >= this.getMaxPag())
+                {
+                    aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+
+                    while (aux != null)
+                    {
+                        if (aux.getLlegada() <= lower && aux.getResidencia() == 1)
+                        {
+                            lower = aux.getLlegada();
+                            lowerID = aux.getNumero();
+                        }
+                        aux = aux.getNextPagina();
+                    }
+
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(lowerID).setResidencia(0);
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1);
+                }
+                else
+                {
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1); // si hay menos qe el max de pag
+                }
+            }
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setAcceso(this.getTiempo() + 1);
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID)
+                    .setNumAcceso(this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getNumAcceso() + 1);
+            this.TiempoPasa();
+        }
+
+        public void LRU(int ProcesoID, int PaginaID)
+        {
+            int counter = 0;
+            int lowerID = 0;
+            int lower = 0;
+            Pagina aux = new Pagina();
+            aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+            if (this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getResidencia() == 0)
+            {
+                while (aux != null)
+                {
+                    if (aux.getResidencia() == 1)
+                    {
+                        counter++;
+                    }
+                    //hacemos crecer al maximo
+                    if (aux.getAcceso() > lower)
+                    {
+                        lower = aux.getLlegada();
+                    }
+                    aux = aux.getNextPagina();
+                }
+                if (counter >= this.getMaxPag())
+                {
+                    aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+
+                    while (aux != null)
+                    {
+                        if (aux.getAcceso() <= lower && aux.getResidencia() == 1)
+                        {
+                            lower = aux.getLlegada();
+                            lowerID = aux.getNumero();
+                        }
+                        aux = aux.getNextPagina();
+                    }
+
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(lowerID).setResidencia(0);
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1);
+                }
+                else
+                {
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1); // si hay menos qe el max de pag
+                }
+            }
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setAcceso(this.getTiempo() + 1);
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID)
+                    .setNumAcceso(this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getNumAcceso() + 1);
+            this.TiempoPasa();
+        }
+
+        public void LFU(int ProcesoID, int PaginaID)
+        {
+            int counter = 0;
+            int lowerID = 0;
+            int lower = 0;
+            Pagina aux = new Pagina();
+            aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+            if (this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getResidencia() == 0)
+            {
+                while (aux != null)
+                {
+                    if (aux.getResidencia() == 1)
+                    {
+                        counter++;
+                    }
+                    //hacemos crecer al maximo
+                    if (aux.getNumAcceso() > lower)
+                    {
+                        lower = aux.getLlegada();
+                    }
+                    aux = aux.getNextPagina();
+                }
+                if (counter >= this.getMaxPag())
+                {
+                    aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+
+                    while (aux != null)
+                    {
+                        if (aux.getNumAcceso() <= lower && aux.getResidencia() == 1)
+                        {
+                            lower = aux.getLlegada();
+                            lowerID = aux.getNumero();
+                        }
+                        aux = aux.getNextPagina();
+                    }
+
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(lowerID).setResidencia(0);
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1);
+                }
+                else
+                {
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1); // si hay menos qe el max de pag
+                }
+            }//FIN siel bit residenciafue 0  
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setAcceso(this.getTiempo() + 1);
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID)
+                    .setNumAcceso(this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getNumAcceso() + 1);
+            this.TiempoPasa();
+        }
+
+        public void NUR(int ProcesoID, int PaginaID)
+        {
+            int counter = 0;
+            int lowerID = 0;
+            int lower = 0;
+            Pagina aux = new Pagina();
+            aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+            if (this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getResidencia() == 0)
+            {
+                while (aux != null)
+                {
+                    if (aux.getResidencia() == 1)
+                    {
+                        counter++;
+                    }
+                    //hacemos crecer al maximo
+                    if (aux.getNumAcceso() > lower)
+                    {
+                        lower = aux.getLlegada();
+                    }
+                    aux = aux.getNextPagina();
+                }
+                if (counter >= this.getMaxPag())
+                {
+                    aux = this.getProcesoByID(ProcesoID).getListaPagina().getPagina();
+
+                    while (aux != null)
+                    {
+                        if (aux.getNumAcceso() <= lower && aux.getResidencia() == 1)
+                        {
+                            lower = aux.getLlegada();
+                            lowerID = aux.getNumero();
+                        }
+                        aux = aux.getNextPagina();
+                    }
+
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(lowerID).setResidencia(0);
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1);
+                }
+                else
+                {
+                    this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setResidencia(1); // si hay menos qe el max de pag
+                }
+            }//FIN siel bit residenciafue 0  
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).setAcceso(this.getTiempo() + 1);
+            this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID)
+                    .setNumAcceso(this.getProcesoByID(ProcesoID).getListaPagina().getPaginaByNumero(PaginaID).getNumAcceso() + 1);
+            this.TiempoPasa();
+        }
+    }
+}
